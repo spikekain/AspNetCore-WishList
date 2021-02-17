a7561be3d688bddf84932afd27701755360027b1
Luis Mendez <luis.mendez@n5now.com>
2021-02-17 15:56:37 -0300

    p

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -2,22 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: HomeController
         public IActionResult Index()
         {
-            return View();
+            return Index();
         }
         public IActionResult Error()
         {
             return Error();
         }
     }
-
 }
